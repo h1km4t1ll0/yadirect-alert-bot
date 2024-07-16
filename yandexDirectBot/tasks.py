703409bb23aaee37adb88624e3e8883365470dbb
hikmatillo <dolgovdaniil007@gmail.com>
2024-07-16 13:08:07 +0700

    fix: balance report

--- yandexDirectBot/tasks.py
+++ yandexDirectBot/tasks.py
@@ -75,6 +75,14 @@ def balance_change_alert():
                         send_message_to_chat(chat.chat_id, message)
                     account.notified = True
                     account.save()
-                elif account_balance.amount > account.min_sum:
+                elif account_balance.amount >= account.min_sum:
+                    for chat in alert.chat.all():
+                        message = (
+                                f'Баланс аккаунта <i>{account_balance.login}</i> пополнен!\n\n'
+                                'Текущий баланс: <b>' + '{:,.0f}'.format(account_balance.amount) + '₽</b>\n'
+                        )
+                        logger.info(f"Notifying {chat.chat_id}")
+                        send_message_to_chat(chat.chat_id, message)
+
                     account.notified = False
                     account.save()
