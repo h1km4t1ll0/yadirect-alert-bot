17f54474d33edb973da80b7698445487175fbdb0
h1km4t1ll0 <dolgovdaniil007@gmail.com>
2025-02-10 01:24:20 +0700

    fix: settings.py

--- yandexDirectBot/settings.py
+++ yandexDirectBot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8&a)j$$*52dz+dp_-94r5&ce9=u*o5r-yn11l3%1734#s&bvx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", True)
 DOMAIN = os.environ.get("DOMAIN", None)
 SERVER_MODE = os.environ.get("SERVER", '') in ["True", True]
 
